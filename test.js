9a5bdee4664c1c8f851053766b8e427f84f4dc3e
Van Tang <van.tang@groovetechnology.com>
2019-04-25 08:59:02 +0700

    Test call API

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 const _ = require('lodash');
+const request = require('request-promise');
 
+var options = {
+  uri: 'http://dummy.restapiexample.com/api/v1/employees',
+  json: true,
+};
+request(options).then(data => {
+  console.log(data.map(emp => emp.employee_name).join(', '))
+});
 console.log('lodash', _.isEmpty(2));
-
-console.log('456')
-console.log('666 123')
+console.log('TEST TRAVIS')
