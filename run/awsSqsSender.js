138d54cf2e470f0c294fe26ab4ee30374011c946
Van Tang <van.tang@groovetechnology.com>
2019-05-14 10:15:32 +0700

    4th Build

--- run/awsSqsSender.js
+++ run/awsSqsSender.js
@@ -6,19 +6,19 @@ const awsSqs = new SQS();
 const queueName = 'test2.fifo';
 const data = [1,2,3,4];
 
-awsSqs.createQueue(queueName).then(res => {
-  var params = {
-    entries: data,
-    groupId: 'test',
-    queueUrl: res.QueueUrl
-  };
-  return awsSqs.sendMessageBatch(params).then(data => {
-    console.log("Sent message with payload", data);
-  });
-});
+// awsSqs.createQueue(queueName).then(res => {
+//   var params = {
+//     entries: data,
+//     groupId: 'test',
+//     queueUrl: res.QueueUrl
+//   };
+//   return awsSqs.sendMessageBatch(params).then(data => {
+//     console.log("Sent message with payload", data);
+//   });
+// });
 
-const queueUrl = 'https://sqs.ap-southeast-1.amazonaws.com/784184982766/domain2.fifo';
+const queueUrl = 'https://sqs.ap-southeast-1.amazonaws.com/784184982766/domain.fifo';
 
 // awsSqs.deleteQueue(queueUrl).then(res => console.log(res));
 
-// awsSqs.getMessageCount(queueUrl).then(res => console.log(res));
+awsSqs.getMessageCount(queueUrl).then(res => console.log(res));
