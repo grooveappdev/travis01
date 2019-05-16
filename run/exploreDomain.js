0c3e573c6ac9ad1beff77d8f8d7d66c679ae7f8f
Van Tang <van.tang@groovetechnology.com>
2019-05-16 15:47:57 +0700

    Update explore script

--- run/exploreDomain.js
+++ run/exploreDomain.js
@@ -3,8 +3,10 @@ const fs = require("fs");
 const _ = require("lodash");
 const ShodanElasticSearch = require("../lib/ShodanElasticSearch");
 const ShodanRequest = require("../lib/ShodanRequest");
+const GoogleBigQuery = require("../lib/BigQuery");
 const SQS = require('../lib/AwsSqs');
 const config = require('../config.json');
+const schema = require('../schema/extra')
 
 const awsSqs = new SQS();
 const shodanES = new ShodanElasticSearch({
@@ -16,6 +18,7 @@ const shodanReq = new ShodanRequest({
   minTime: 2000,
   maxConcurrent: 8
 });
+const bigQuery = new GoogleBigQuery();
 
 const EDIT_PROPERTIES = [
   'CategoryRank.Rank',
@@ -27,18 +30,41 @@ const queueURL = `${config.queueHost}/${config.queueDomainName}`;
 awsSqs.receiveMessage(queueURL).then(message => {
   console.log('receive', message);
   const domainChunk = awsSqs.parseMessage(message.Body);
-  return Promise.all(domainChunk.map(chunk => {
-    return shodanReq.exploreDomain(chunk.domain, EDIT_PROPERTIES).then(extraInfo => {
-      extraInfo.hostId = chunk.hostId;
-      return extraInfo;
+  bigQuery.initClient()
+    .then(() => {
+      return bigQuery.createTableIfNotExist('testing', 'extra', schema);
     })
-  })).then(partialHostList => {
-    return shodanES.batchUpdate(partialHostList, config.esIndexName, 'host');
-  }).then(() => {
-    console.log('update completed')
-    message.ack().then(() => {
-      console.log('message ACK');
-      process.exit(0);
+    .then(() => {
+      return Promise.all(domainChunk.map(chunk => {
+        return shodanReq.exploreDomain(chunk.domain, EDIT_PROPERTIES).then(extraInfo => {
+          extraInfo.hostId = chunk.hostId;
+          return extraInfo;
+        })
+      }))
+    })
+    .then(extraInfoList => {
+      const bigQueryData = bigQuery.parseBigQueryData(extraInfoList);
+      return bigQuery.insertDataAsChunk('testing', 'extra', bigQueryData, 100).then(res => {
+        console.log(res);
+        fs.writeFile(
+          "./bigquery.json",
+          JSON.stringify(res),
+          "utf8",
+          () => console.log("done bigquery.json")
+        );
+      }).catch(err => {
+        fs.writeFile(
+          "./bigquery.json",
+          JSON.stringify(err),
+          "utf8",
+          () => console.log("done bigquery.json")
+        );
+      })
+    }).then(() => {
+      console.log('update completed')
+      message.ack().then(() => {
+        console.log('message ACK');
+        process.exit(0);
+      });
     });
-  });
 }).catch(err => console.log(err.message))
